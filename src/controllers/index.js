98c1c82d2a74ffd622ca2715ea1ce7c9bf62afeb
95Rawan <youlostme17@hotmail.com>
2019-08-07 11:28:11 +0300

    downloading eslint

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,17 +1,15 @@
-const express = require("express");
+const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
 
-const home = require("./home");
+const home = require('./home')
 
-const path = require("path");
+router.use(express.json())
 
-router.use(express.json());
+router.use(express.urlencoded())
 
-router.use(express.urlencoded());
+const painting = require('./painting')
+router.get('/painting', painting.get)
 
-const painting = require("./painting")
-router.get("/painting", painting.get)
-
-router.get("/", home.get);
-module.exports = router;
+router.get('/', home.get)
+module.exports = router
